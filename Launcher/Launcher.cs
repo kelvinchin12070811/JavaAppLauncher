bf928a57cdaee7e68495f1463b6ff79b53d254eb
Kelvin Chin <chinkaiwen@yahoo.com>
2019-09-14 11:38:12 +0800

    Add project files.

---
+++ Launcher/Launcher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Launcher
+{
+    class Launcher
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
