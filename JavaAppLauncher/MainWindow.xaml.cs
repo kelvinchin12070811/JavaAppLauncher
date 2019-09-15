d921801200ffaccf6050b185742cfe58775ee2d1
Kelvin Chin <chinkaiwen@yahoo.com>
2019-09-15 21:11:50 +0800

    Compleated basic layout for launcher maker

--- JavaAppLauncher/MainWindow.xaml.cs
+++ JavaAppLauncher/MainWindow.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿//***********************************************************************************************************
+//This Source Code Form is subject to the terms of the Mozilla Public
+//License, v. 2.0. If a copy of the MPL was not distributed with this
+//file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//***********************************************************************************************************
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +29,20 @@ namespace JavaAppLauncher
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void BtnBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog jarSelector = new OpenFileDialog();
+            jarSelector.Filter = "Java runable jar file (*.jar)|*.jar|All file (*.*)|*.*";
+            jarSelector.DefaultExt = ".jar";
+
+            Nullable<bool> result = jarSelector.ShowDialog();
+            if (result != null)
+            {
+                if (result == false) return;
+                jarFilename.Text = jarSelector.FileName;
+            }
         }
     }
 }
