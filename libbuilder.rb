48cabec3009099351cd955c71273226ffcb9153d
Kelvin Chin <chinkaiwen@yahoo.com>
2020-06-11 16:53:01 +0800

    reset for attempt_2

--- libbuilder.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/env/bin ruby
-
-config = 'Debug'
-platform = 'Win32'
-
-config = ARGV[0] if ARGV.length >= 1
-platform = ARGV[1] if ARGV.length >= 2
-
-system "devenv /Build \"#{ config }|#{ platform }\" ziplib/Source/ZipLib/ZipLib.vcxproj"
